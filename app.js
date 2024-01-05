67bed3e709c176904d96a66a4de6caf03eb5a60b
aayushwrld <aayush.arora@kalvium.community>
2024-01-05 16:04:08 +0530

    Final

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const button = document.getElementById("button");
 button.onclick = () => {
   location.href = "./game.html";
 };
+
+let scoreBoard = localStorage.getItem("score")
+let spanScore = document.getElementById("scoreId")
+
+spanScore.textContent = `${scoreBoard}!`
--- game.js
+++ game.js
@@ -1,3 +1,101 @@
+let gameBody = document.querySelector("#game-body");
+let count = 0;
+let score = 0;
+let lives = 5;
+let marginChange = 0;
+let barLife = document.getElementById("max-lives");
+let ops2;
+let zombImage = document.querySelector("zombie-image")
+let shotAudio = new Audio("./assets/shotgun.wav")
+let bgmAudio = new Audio("./assets/bgm.mp3")
+
+bgmAudio.play()
+bgmAudio.loop = true
+gameBody.onclick = () =>{
+    shotAudio.play()
+}
+
+function zombieImage() {
+    if(lives==0){
+        window.open("game-over.html","_self")
+    }
+    ops2 = Math.ceil(Math.floor()*3)
+    let img = document.createElement("img");
+    let marginValue = Math.ceil(Math.random() * 90);
+    img.style.marginLeft = `${marginValue}%`;
+    img.setAttribute("class", "zombie-image");
+    let operator = Math.ceil(Math.random()*5)
+    switch(operator){
+        case 1:
+            img.setAttribute("src", "./assets/zombie-1.png");
+            break
+        case 2:
+            img.setAttribute("src", "./assets/zombie-2.png");
+            break
+        case 3:
+            img.setAttribute("src", "./assets/zombie-3.png");
+            break
+        case 4:
+            img.setAttribute("src", "./assets/zombie-4.png");
+            break
+        case 5:
+            img.setAttribute("src", "./assets/zombie-5.png");
+            break
+    }
+let ops = Math.ceil(Math.random()*3)+2
+img.style.animationDuration = `${ops}s`
+img.style.width = "100px";
+gameBody.appendChild(img);
+count = 0;
+img.onclick = () => {
+    count ++
+    score ++
+    localStorage.setItem("score",score)
+    gameBody.removeChild(img);
+    zombieImage();
+};
+
+setTimeout(()=>{
+    if (count == 0){
+        gameBody.removeChild(img)   
+        zombieImage()
+        lives --;
+        console.log(lives)
+        marginChange += 10;
+        barLife.style.marginLeft = `${marginChange}%`
+        barLife.style.marginRight = `${marginChange}%`
+        barLife.style.width = `${20*lives}%`
+    }
+},`${ops*1000}`)
+
+}
+let time = 20;
+let timer = document.getElementById("timer");
+let changeTime;
+
+function startTimer(){
+    time = 60;
+    timer.textContent = time;
+    changeTime = setInterval(()=>{
+        time--;
+        timer.textContent = time;
+        if (time == 0){
+            window.open("win.html", "_self");
+        }
+    },1000)
+}
+localStorage.setItem("score",score)
+
+startTimer()
+zombieImage();
+
+
+
+
+
+
+
+
 // Iteration 1: Declare variables required for this game
 
 // Iteration 1.2: Add shotgun sound
